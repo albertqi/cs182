3f4cb03dab0c5739df6e5f574f82a126d6d8d24f
Albert Qi <albertqi03@gmail.com>
2023-11-12 14:26:11 -0500

    [pset3] Implement policy_iteration

--- pset3/pset3.py
+++ pset3/pset3.py
@@ -11,36 +11,36 @@ import matplotlib.pyplot as plt
 ### Package Imports ###
 
 #### Coding Problem Set General Instructions - PLEASE READ ####
-# 1. Unlike previous psets, this code does not need to be submitted; there is no autograder
-# 2. This code goes with Problem 2: Employment Status, on this pset
+# 1. Unlike previous psets, this code does not need to be submitted; there is no autograder.
+# 2. This code goes with Problem 2: Employment Status, on this pset.
 # 3. This starter code has been provided to you, feel free to use it (or not, if you want to code something different) however you
 #    see fit. Change variables, solve the question in another way, however you need to best understand the question and your code.
 #    This coding problem can be written in a variety of different ways, this code is only a rough sketch of what your code could
 #    look like. We encourage you to change it if you need to.
-# 4. Make sure you write the optimal policies your code determines and copy your code and graphs onto your written submission
+# 4. Make sure you write the optimal policies your code determines and copy your code and graphs onto your written submission.
 
 
-n_states = 3  # 0 is Safely Employed (SE), 1 is PIP, 2 is Unemployed (UE)
-n_actions = 2  # 0 is Code, 1 is Netflix
+n_states = 3  #  0 is Safely Employed (SE), 1 is PIP, 2 is Unemployed (UE).
+n_actions = 2  # 0 is Code, 1 is Netflix.
 
 t_p = np.zeros((n_states, n_actions, n_states))
-# Transition Probabilities: These are represented as a 3-dimensional array
-# t_p[s_1, a, s_2] = p indicates that beginning from state s_1 and taking action a will result in state s_2 with probability p
-t_p[0, 0, 0] = 1  # t_p[SE, Code, SE]      = 1
+# Transition Probabilities: These are represented as a 3-dimensional array.
+# t_p[s_1, a, s_2] = p indicates that beginning from state s_1 and taking action a will result in state s_2 with probability p.
+t_p[0, 0, 0] = 1  #     t_p[SE, Code, SE]      = 1
 t_p[0, 1, 0] = 1 / 4  # t_p[SE, Netflix, SE]   = 1 / 4
 t_p[0, 1, 1] = 3 / 4  # t_p[SE, Code, PIP]     = 3 / 4
 t_p[1, 0, 0] = 1 / 4  # t_p[PIP, Code, SE]     = 1 / 4
 t_p[1, 0, 1] = 3 / 4  # t_p[PIP, Code, PIP]    = 3 / 4
 t_p[1, 1, 1] = 7 / 8  # t_p[PIP, Netflix, PIP] = 7 / 8
 t_p[1, 1, 2] = 1 / 8  # t_p[PIP, Netflix, UE]  = 1 / 8
-# all other transition probabilities are 0
+# All other transition probabilities are 0.
 
 r = np.zeros((n_states, n_actions))
-# Reward values: These are represented as a 2-dimensional array
-# r[s, a] = val indicates that taking at state s, taking action a will give a reward of val
-r[0, 0] = 4  # r[SE, Code]       = 4
+# Reward Values: These are represented as a 2-dimensional array.
+# r[s, a] = val indicates that taking at state s, taking action a will give a reward of val.
+r[0, 0] = 4  #  r[SE, Code]       = 4
 r[0, 1] = 10  # r[SE, Netflix]    = 10
-r[1, 0] = 4  # r[PIP, Code]      = 4
+r[1, 0] = 4  #  r[PIP, Code]      = 4
 r[1, 1] = 10  # r[PIP, Netflix]   = 10
 
 
@@ -51,24 +51,43 @@ def policy_iteration(gamma):
     What this function returns is up to you and how you want to determine the sum of utilities at each iteration in the plots.
     """
 
-    theta = 1e-5  # define a theta that determines if the change in utilities from iteration to iteration is "small enough"
+    theta = 1e-5  # Define a theta that determines if the change in utilities from iteration to iteration is "small enough".
 
     policy = np.zeros(
         n_states, dtype=int
-    )  # define your policy, which begins as Netflix regardless of state
+    )  # Define your policy, which begins as Netflix regardless of state.
 
     while True:
         # Policy Evaluation
-        # TODO: Policy Evaluation code here
+        V = np.zeros(n_states, dtype=float)
+        while True:
+            V_next = np.zeros(n_states, dtype=float)
+            for state in range(n_states):
+                action = policy[state]
+                for new_state in range(n_states):
+                    V_next[state] += t_p[state, action, new_state] * (
+                        r[state, action] + gamma * V[new_state]
+                    )
+            if np.max(np.abs(V - V_next)) < theta:
+                V = V_next
+                break
+            V = V_next
 
         # Policy Iteration
-        # TODO: Policy Iteration code here
+        policy_next = np.zeros(n_states, dtype=int)
+        for state in range(n_states):
+            action_vals = np.zeros(n_actions, dtype=float)
+            for action in range(n_actions):
+                for new_state in range(n_states):
+                    action_vals[action] += t_p[state, action, new_state] * (
+                        r[state, action] + gamma * V[new_state]
+                    )
+            policy_next[state] = np.argmax(action_vals)
 
-        # Policy Change check
-        policy_stable = True
-        # TODO: Determine if policy has changed between iterations
-        if policy_stable:
-            break
+        # Policy Change Check
+        if np.all(policy == policy_next):
+            return policy
+        policy = policy_next
 
 
 def value_plots():
@@ -81,7 +100,7 @@ def value_plots():
 
     iterations = range(0, 50)
 
-    # you will need to find a way to calculate the cumulative utility values for policy 1 and policy 2
+    # You will need to find a way to calculate the cumulative utility values for policy 1 and policy 2.
     plt.plot(iterations, p1_vals, label="Policies for gamma = 0.9")
     plt.plot(iterations, p2_vals, label="Policies for gamma = 0.8")
     plt.xlabel("Iterations")
@@ -94,8 +113,8 @@ def value_plots():
 if __name__ == "__main__":
     policy_iteration(
         0.9
-    )  # policy iteration to verify your answer from problem 2 part c, with gamma = 0.9
-    policy_iteration(0.8)  # policy iteration for problem 2 part d, with gamma = 0.8
+    )  # Policy iteration to verify your answer from problem 2 part c, with gamma = 0.9.
+    policy_iteration(0.8)  # Policy iteration for problem 2 part d, with gamma = 0.8.
 
     value_plots()
     # You will need to find some way to get the total utility values to the value_plots function. For example, you could pass
